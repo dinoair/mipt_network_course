d4ce4a031c915f692cb6f9574d2a936f1aad4392
dinoair <cool.airatik20@yandex.ru>
2024-03-12 23:36:15 +0300

    Fix bug

--- url_image/views.py
+++ url_image/views.py
@@ -10,13 +10,13 @@ from .backend import api
 
 def index(request):
 
-    # default_portret_url = "https://image.freepik.com/free-photo/young-extraterrestrial-woman-s-portret_144627-3464.jpg"
+    # default_portrait_url = "https://image.freepik.com/free-photo/young-extraterrestrial-woman-s-portrait_144627-3464.jpg"
     # default_style_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrXW-y7mU0DQW9jt7kCIXV3BQXO5CAD-Glhw&usqp=CAU"
     
-    default_portret_url = "https://example1.jpg"
+    default_portrait_url = "https://example1.jpg"
     default_style_url = "https://example2.jpg"
     
-    portret_err = False
+    portrait_err = False
     style_err = False
 	# if this is a POST request we need to process the form data
     if request.method == 'POST':
@@ -27,10 +27,10 @@ def index(request):
             # print("\n")
             # process the data in form.cleaned_data as required
             try:
-                portret, portret64 = api.get_img_and_base64(form.cleaned_data['portret_url'])
+                portrait, portrait64 = api.get_img_and_base64(form.cleaned_data['portrait_url'])
             except Exception as e:
-                portret_err = True
-                # print("portret err ", e, portret_err)
+                portrait_err = True
+                print("portrait err ", e, portrait_err)
 
             try:
                 style, style64 = api.get_img_and_base64(form.cleaned_data['style_url'])
@@ -41,52 +41,52 @@ def index(request):
                 # print("\nerror in get_img_and_base64\n")
                 # form.cleaned_data['style_url'] = "Sorry! Image can't be loaded from that link);"
             
-            if not portret_err and not style_err:
+            if not portrait_err and not style_err:
                 # print("\nmask\n")
-                mask, mask64 = api.get_person_mask_and_mask64(portret)
-                result64 = api.merge_style_and_person(mask, portret, style)
-                # stylized64 = sty.get_stylized_portret(style, portret, mask)
-                context = {'form': form, 'portret64': portret64, "style64": style64,\
+                mask, mask64 = api.get_person_mask_and_mask64(portrait)
+                result64 = api.merge_style_and_person(mask, portrait, style)
+                # stylized64 = sty.get_stylized_portrait(style, portrait, mask)
+                context = {'form': form, 'portrait64': portrait64, "style64": style64,\
                 "mask64": mask64, "new_imgs": True, "load_static_examples": False,\
-                "portret_err": False, "style_err": False, "result64": result64,\
+                "portrait_err": False, "style_err": False, "result64": result64,\
                 "not_valid_form": False}
                 return render(request, 'url_image/index.html', context)
             else:
-                context = {'form': form, "portret_err": portret_err, "style_err": style_err,\
+                context = {'form': form, "portrait_err": portrait_err, "style_err": style_err,\
                 "load_static_examples": False, "new_imgs": False, "not_valid_form": False}
                 return render(request, 'url_image/index.html', context)
         else:
-            context = {'form': form, "portret_err": False, "style_err": False, \
+            context = {'form': form, "portrait_err": False, "style_err": False, \
             "load_static_examples": False, "new_imgs": False, "not_valid_form": True}
             return render(request, 'url_image/index.html', context)
 
-            # if portret_err and not style_err:
-            #     response_form = UrlsForm(initial={'portret_url': "",\
+            # if portrait_err and not style_err:
+            #     response_form = UrlsForm(initial={'portrait_url': "",\
             #     "style_url": form.cleaned_data['style_url']})
-            #     context = {'form': response_form, "portret_ok": not portret_err, "style_ok": not style_err}
+            #     context = {'form': response_form, "portrait_ok": not portrait_err, "style_ok": not style_err}
             #     return render(request, 'url_image/index.html', context)
             
-            # if portret_err == 0 and style_err == 1:
-            #     response_form = UrlsForm(initial={'portret_url': form.cleaned_data['portret_url'],\
+            # if portrait_err == 0 and style_err == 1:
+            #     response_form = UrlsForm(initial={'portrait_url': form.cleaned_data['portrait_url'],\
             #     "style_url": ""})
             #     context = {'form': response_form, "new_imgs": False}
             #     return render(request, 'url_image/index.html', context)
 
-            # if portret_err == 1 and style_err == 1:
+            # if portrait_err == 1 and style_err == 1:
             #     response_form = UrlsForm()
             #     context = {'form': response_form, "new_imgs": False}
             #     return render(request, 'url_image/index.html', context)
 
-            # context = {'form': form, 'portret64': portret64, "style64": style64,\
+            # context = {'form': form, 'portrait64': portrait64, "style64": style64,\
             # "mask64": mask64, "stylized64": stylized64}
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = UrlsForm(initial={'portret_url': default_portret_url,\
+        form = UrlsForm(initial={'portrait_url': default_portrait_url,\
                         "style_url": default_style_url})
-        # stylized64 = sty.get_stylized_portret(style, portret, mask)
+        # stylized64 = sty.get_stylized_portrait(style, portrait, mask)
         context = {'form': form, "load_static_examples": True, "new_imgs": False,\
-        "portret_err": False, "style_err": False, "not_valid_form": False}
+        "portrait_err": False, "style_err": False, "not_valid_form": False}
 
         return render(request, 'url_image/index.html', context)
 
